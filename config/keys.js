349868559dff8bad438f61ba26954f083fa9b05a
Carl Pablico <rudolfpablico@gmail.com>
2020-08-15 13:20:19 +0800

    deployed to heroku

---
+++ config/keys.js
@@ -0,0 +1,4 @@
+module.exports = {
+  mongoURI:
+    "mongodb+srv://mernadmin1:abcd1234@cluster0.vou01.mongodb.net/shoppinglist?retryWrites=true&w=majority",
+};
--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 const express = require("express");
 const mongoose = require("mongoose");
 const bodyParser = require("body-parser");
+const path = require("path");
 
 const items = require("./routes/api/items");
 
@@ -24,6 +25,15 @@ mongoose
 
 //Use Routes
 app.use("/api/items", items);
+
+//Serve static folder if production
+if (process.env.NODE_ENV === "production") {
+  app.use(express.static("client/build"));
+
+  app.get("*", (req, res) => {
+    res.sendFile(path.resolve(__dirname, "client", "build", "index.html"));
+  });
+}
 
 //Setup Port
 const port = process.env.PORT || 5000;
